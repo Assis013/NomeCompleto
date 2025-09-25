d6de81bc1a36b2e73d632bcbb5692b9768a07492
Leticia Gabriely <leticiagabriely629@gmail.com>
2025-09-24 21:56:53 -0300

    Finalizar

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Nome fixo
+        string nome = "Leticia";
+        string sobrenome = "Assis";
+
+        // Saídas
+        Console.WriteLine("Nome completo: " + nome + " " + sobrenome);
+        Console.WriteLine("Nome de catálogo: " + sobrenome.ToUpper() + ", " + nome);
+    }
+}
